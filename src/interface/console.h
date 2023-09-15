0fc4456ae486e1accdfb346c7966f024bab10bf5
verenach <verenach@student.21-school.ru>
2023-09-15 16:30:47 +0700

    добавила консоль

---
+++ src/interface/console.h
@@ -0,0 +1,17 @@
+#ifndef PARALLELS_CONSOLE_H
+#define PARALLELS_CONSOLE_H
+
+#include <iostream>
+
+#include "../gauss/gauss.h"
+// #include "..matrix/matrix.h"
+
+namespace Parallels {
+class Console {
+ public:
+ private:
+  Gauss gauss_thread_;
+};
+};  // namespace Parallels
+
+#endif  // PARALLELS_CONSOLE_H
---
+++ src/interface/main.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "console.h"
+
+int main(int argc, char* argv[]) {
+  if (argc) {
+  } else {
+    std::cout << "Enter method: ant, gauss or winograd:\n";
+  }
+  return 0;
+}
