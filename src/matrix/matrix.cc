ee37a21582c60ca57b429f69ecc4c56d7b04a2c2
KristinaSaraeva <kristinasaraeva660@gmail.com>
2023-09-15 15:38:21 +0700

    started

---
+++ src/matrix/matrix.cc
@@ -0,0 +1,13 @@
+#ifndef PARALLELS_MATRIX_H
+#define PARALLELS_MATRIX_H
+
+#include <vector>
+
+namespace Parallels {
+
+class Matrix {
+  using Matrix = std::vector<std::vector<double>>;
+};
+};  // namespace Parallels
+
+#endif  // PARALLELS_MATRIX_H
