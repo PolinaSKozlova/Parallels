4139fd940d32cb5f5fc1302950a742cc33c1853a
verenach <verenach@student.21-school.ru>
2023-09-21 18:00:34 +0700

    check

--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,28 @@
 #include <iostream>
 
-int main(argv[], argc) {
-  if (argv.size()) {
-    ShowConsole(argv[0]);
-  } else {
-    ShowConsole();
+#include "winograd/winograd2.h"
+
+int main() {
+  Parallels::Matrix a(2, 3);
+  Parallels::Matrix b(3, 1);
+  std::vector<double> aa{4, 3, 1, 9, 0, 2};
+  std::vector<double> bb{1, 2, 3};
+  a.FillMatrix(aa);
+  b.FillMatrix(bb);
+  std::cout << "Matrix a:" << std::endl;
+  a.PrintMatrix();
+  std::cout << "Matrix b:" << std::endl;
+  b.PrintMatrix();
+  Parallels::Winograd w;
+  try {
+    Parallels::Matrix res = w.MultiplyMatrices(a, b);
+    std::cout << "Matrix res:" << std::endl;
+    res.PrintMatrix();
+    std::cout << "getter:" << std::endl;
+    w.GetResult().PrintMatrix();
+  } catch (std::invalid_argument &e) {
+    std::cout << e.what() << std::endl;
   }
+
   return 0;
 }
