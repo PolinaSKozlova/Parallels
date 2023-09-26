e9a061d5e5bc036579064587e592ce3035c7917e
verenach <verenach@student.21-school.ru>
2023-09-26 16:18:23 +0700

    add constructor with size parametre

--- src/threads/threads_pool.h
+++ src/threads/threads_pool.h
@@ -1,36 +1,50 @@
 #ifndef PARALLELS_THREADS_POOLS_H
 #define PARALLELS_THREADS_POOLS_H
 
+#include <atomic>
 #include <condition_variable>
 #include <functional>
 #include <mutex>
 #include <queue>
 #include <thread>
 #include <vector>
-#include <atomic>
 
 #include "thread_queue.h"
 
 namespace Parallels {
 class ThreadsPool {
+  class JoinThreads {
+   public:
+    explicit JoinThreads(std::vector<std::thread>& threads)
+        : threads_(threads) {}
+    ~JoinThreads() {
+      for (unsigned long i = 0; i < threads_.size(); ++i) {
+        if (threads_[i].joinable()) {
+          threads_[i].join();
+        }
+      }
+    }
 
-class JoinThreads {
-public:
-  explicit JoinThreads(  std::vector<std::thread>& threads) : threads_(threads){ }
-  ~JoinThreads() {
-    for (unsigned long i=0;i<threads_.size();++i) {
-      if (threads_[i].joinable()){
-      threads_[i].join();
-    }
-    }
-  }
-  private:
-  std::vector<std::thread>& threads_;
-};
+   private:
+    std::vector<std::thread>& threads_;
+  };
 
  public:
-  ThreadsPool(): done_(false),joiner_(threads_) {
-    unsigned const threads_count = std::thread::hardware_concurrency(); // think about
+  ThreadsPool() : done_(false), joiner_(threads_) {
+    unsigned const threads_count =
+        std::thread::hardware_concurrency();  // think about
+    try {
+      for (unsigned i = 0; i < threads_count; ++i) {
+        threads_.push_back(std::thread(&ThreadsPool::Run, this));
+      }
+    } catch (...) {
+      done_ = true;
+      throw std::runtime_error("ThreadsPool execution exception");
+    }
+  }
+  ThreadsPool(unsigned const threads_count) : done_(false), joiner_(threads_) {
+    // unsigned const threads_count = std::thread::hardware_concurrency(); //
+    // think about
     try {
       for (unsigned i = 0; i < threads_count; ++i) {
         threads_.push_back(std::thread(&ThreadsPool::Run, this));
@@ -41,9 +55,7 @@ public:
     }
   }
 
-  ~ThreadsPool() {
-    done_ = true;
-  };
+  ~ThreadsPool() { done_ = true; };
 
   template <typename F>
   void AddVoidTask(F func) {
@@ -51,31 +63,27 @@ public:
     work_queue_.Push(std::function<void()>(func));
   }
 
-
  private:
-
   // std::queue<std::function<void()>> tasks_;
   // std::mutex mutex_;
   // std::condition_variable condition_;
   // bool stop{};
 
-  std::atomic <bool> done_;
+  std::atomic<bool> done_;
   ThreadSafeQueue<std::function<void()>> work_queue_;
   std::vector<std::thread> threads_;
   JoinThreads joiner_;
 
- void Run() {
+  void Run() {
     while (!done_) {
-  std::function<void()> task;
-  if(work_queue_.TryPop(task)){
-    task();
-  } else {
-    std::this_thread::yield();
+      std::function<void()> task;
+      if (work_queue_.TryPop(task)) {
+        task();
+      } else {
+        std::this_thread::yield();
+      }
     }
-  }    
-}
-
-
+  }
 };
 };  // namespace Parallels
 
