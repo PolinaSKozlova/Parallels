7b2fa8ad622405fb0e467f4845dd6b461510aa6b
verenach <verenach@student.21-school.ru>
2023-11-03 17:29:40 +0700

    add 3 versions for winograd

--- src/winograd/winograd.cc
+++ src/winograd/winograd.cc
@@ -36,45 +36,105 @@ Matrix Winograd::MultiplyMatricesInParallels(unsigned int threads_amount) {
       CountResultMatrix(start, end);
       std::lock_guard<std::mutex> lock(mtx);
     }));
-    // threads.at(i - 1) = std::move(std::thread(CountResultMatrix, start, end));
-
+    // threads.at(i - 1) = std::move(std::thread(CountResultMatrix, start,
+    // end));
   }
   for (auto& th : threads) {
-      if (th.joinable()) th.join();
-    }
+    if (th.joinable()) th.join();
+  }
   return GetResultMatrix();
 }
+
+// Matrix Winograd::MultiplyMatricesInPipeline() {
+//   if (!CheckSize(wd_.a_.GetCols(), wd_.b_.GetRows()))
+//     throw std::invalid_argument("Matrices are not compatible!");
+//   SetMatrix(wd_.a_, wd_.b_);
+//   std::packaged_task<void()> count_row_factor(
+//       std::bind(&Winograd::CountRowFactors, this));
+//   std::packaged_task<void()> count_column_factor(
+//       std::bind(&Winograd::CountColumnFactors, this));
+
+//   std::future<void> row_future = count_row_factor.get_future();
+//   std::future<void> column_future = count_column_factor.get_future();
+
+//   std::thread row_thread(std::move(count_row_factor));
+//   std::thread column_thread(std::move(count_column_factor));
+
+//   row_future.wait();
+//   column_future.wait();
+
+//   // std::vector<double> row_factor = row_future.get();
+//   // std::vector<double> column_factor = column_future.get();
+
+//   row_thread.join();
+//   column_thread.join();
+
+//   std::thread result_thread(
+//       [&]() { CountResultMatrix(0, wd_.result_.GetRows()); });
+
+//   result_thread.join();
+
+//   return GetResultMatrix();
+// }
+
+// Matrix Winograd::MultiplyMatricesInPipeline() {
+//   if (!CheckSize(wd_.a_.GetCols(), wd_.b_.GetRows()))
+//     throw std::invalid_argument("Matrices are not compatible!");
+//   SetMatrix(wd_.a_, wd_.b_);
+
+//   std::packaged_task<void()> task([&]() {
+//     CountRowFactors();
+//     CountColumnFactors();
+//   });
+
+//   std::future<void> task_future = task.get_future();
+//   std::thread work_thread(std::move(task));
+
+//   work_thread.join();
+
+//   std::thread main_thread([&]() { CountResultMatrix(0, wd_.a_.GetRows()); });
+
+//   main_thread.join();
+
+//   return GetResultMatrix();
+// }
 
 Matrix Winograd::MultiplyMatricesInPipeline() {
   if (!CheckSize(wd_.a_.GetCols(), wd_.b_.GetRows()))
     throw std::invalid_argument("Matrices are not compatible!");
   SetMatrix(wd_.a_, wd_.b_);
-  std::packaged_task<void()> count_row_factor(
-      std::bind(&Winograd::CountRowFactors, this));
-  std::packaged_task<void()> count_column_factor(
-      std::bind(&Winograd::CountColumnFactors, this));
 
-  std::future<void> row_future = count_row_factor.get_future();
-  std::future<void> column_future =
-      count_column_factor.get_future();
+  auto factors = [&]() {
+    CountRowFactors();
+    CountColumnFactors();
+    {
+      std::lock_guard<std::mutex> lock(queue_mutex_);
+      for (int i = 0; i < wd_.a_.GetRows(); i++) {
+        task_queue_.push(i);
+      }
+      done_ = true;
+    }
+    cv_.notify_all();
+  };
 
-  std::thread row_thread(std::move(count_row_factor));
-  std::thread column_thread(std::move(count_column_factor));
+  auto result = [&]() {
+    while (true) {
+      int task{};
+      {
+        std::unique_lock<std::mutex> lock(queue_mutex_);
+        cv_.wait(lock, [this] { return !task_queue_.empty() || done_; });
+        if (task_queue_.empty()) break;
+        task = task_queue_.front();
+        task_queue_.pop();
+      }
+      CountResultMatrix(task, task + 1);
+    }
+  };
 
-  row_future.wait();
-  column_future.wait();
+  std::thread factors_thread(factors);
+  std::thread result_thread(result);
 
-  // std::vector<double> row_factor = row_future.get();
-  // std::vector<double> column_factor = column_future.get();
-
-  row_thread.join();
-  column_thread.join();
-
-
-  std::thread result_thread(
-      [&]() {
-        CountResultMatrix(0,wd_.result_.GetRows());
-      });
+  factors_thread.join();
   result_thread.join();
 
   return GetResultMatrix();
@@ -92,29 +152,30 @@ void Winograd::CountRowFactors() {
   for (int i = 0; i < wd_.a_.GetRows(); i++) {
     wd_.row_factor_[i] = wd_.a_.GetMatrix()[i][0] * wd_.a_.GetMatrix()[i][1];
     for (int j = 1; j < wd_.half_size_; ++j) {
-      wd_.row_factor_[i] = wd_.row_factor_[i] +
-                      wd_.a_.GetMatrix()[i][2 * j] * wd_.a_.GetMatrix()[i][2 * j + 1];
-                    
+      wd_.row_factor_[i] =
+          wd_.row_factor_[i] +
+          wd_.a_.GetMatrix()[i][2 * j] * wd_.a_.GetMatrix()[i][2 * j + 1];
     }
   }
 }
 
 void Winograd::CountColumnFactors() {
-    wd_.column_factor_.resize(wd_.b_.GetCols());
+  wd_.column_factor_.resize(wd_.b_.GetCols());
   for (int i = 0; i < wd_.b_.GetCols(); i++) {
     wd_.column_factor_[i] = wd_.b_.GetMatrix()[0][i] * wd_.b_.GetMatrix()[1][i];
     for (int j = 1; j < wd_.half_size_; ++j) {
-      wd_.column_factor_[i] = wd_.column_factor_[i] + wd_.b_.GetMatrix()[2 * j][i] *
-                                                wd_.b_.GetMatrix()[2 * j + 1][i];
-                                                  
+      wd_.column_factor_[i] =
+          wd_.column_factor_[i] +
+          wd_.b_.GetMatrix()[2 * j][i] * wd_.b_.GetMatrix()[2 * j + 1][i];
     }
   }
 }
 
-void Winograd::CountResultMatrix(const int start, const int end)  {
+void Winograd::CountResultMatrix(const int start, const int end) {
   for (int i = start; i < end; ++i) {
     for (int j = 0; j < wd_.b_.GetCols(); ++j) {
-      wd_.result_.GetMatrix()[i][j] = - wd_.row_factor_[i] - wd_.column_factor_[j];
+      wd_.result_.GetMatrix()[i][j] =
+          -wd_.row_factor_[i] - wd_.column_factor_[j];
       for (int k = 0; k < wd_.half_size_; ++k) {
         wd_.result_.GetMatrix()[i][j] +=
             (wd_.a_.GetMatrix()[i][2 * k] + wd_.b_.GetMatrix()[2 * k + 1][j]) *
@@ -128,12 +189,12 @@ void Winograd::CountResultMatrix(const int start, const int end)  {
   }
 }
 
-void Winograd::CountOddRows(const int start,
-                            const int end) const {
+void Winograd::CountOddRows(const int start, const int end) const {
   for (int i = start; i < end; ++i) {
     for (int j = 0; j < wd_.b_.GetCols(); ++j) {
-      wd_.result_.GetMatrix()[i][j] += wd_.a_.GetMatrix()[i][wd_.a_.GetCols() - 1] *
-                                  wd_.b_.GetMatrix()[wd_.a_.GetCols() - 1][j];
+      wd_.result_.GetMatrix()[i][j] +=
+          wd_.a_.GetMatrix()[i][wd_.a_.GetCols() - 1] *
+          wd_.b_.GetMatrix()[wd_.a_.GetCols() - 1][j];
     }
   }
 }
--- src/winograd/winograd.h
+++ src/winograd/winograd.h
@@ -1,6 +1,10 @@
 #ifndef PARALLELS_WINOGRAD_H
 #define PARALLES_WINOGRAD_H
 
+#include <future>
+#include <mutex>
+#include <queue>
+
 #include "../matrix/matrix.h"
 
 namespace Parallels {
@@ -20,8 +24,9 @@ class Winograd {
  public:
   Winograd() = default;
   explicit Winograd(const Matrix& a, const Matrix& b) : wd_{a, b} {
-    if (!CheckSize(wd_.a_.GetCols(), wd_.b_.GetRows())){
-      throw std::invalid_argument("Matrices are not compatible!");}
+    if (!CheckSize(wd_.a_.GetCols(), wd_.b_.GetRows())) {
+      throw std::invalid_argument("Matrices are not compatible!");
+    }
     SetMatrix(wd_.a_, wd_.b_);
   }
   ~Winograd() = default;
@@ -36,13 +41,20 @@ class Winograd {
   void SetMatrix(const Matrix& a, const Matrix& b);
   void CountRowFactors();
   void CountColumnFactors();
-  void CountResultMatrix(const int start,
-                         const int end) ;
+  void CountBothFactors();
+  void CountResultMatrix(const int start, const int end);
+  void CountResultMatrix(const int i, const int j,
+                         std::pair<double, double> factors);
   void CountOddRows(const int start, const int end) const;
   bool CheckSize(const int a_cols, const int b_rows) const noexcept;
   bool IsOddMatrix(const int a_cols) const;
   WinogradData wd_{};
-
+  std::queue<int> task_queue_;  // Очередь задач. Здесь будут храниться индексы
+                                // строк для обработки.
+  std::mutex
+      queue_mutex_;  // Мьютекс для синхронизации доступа к очереди задач.
+  std::condition_variable cv_;  // Переменная условия для синхронизации потоков.
+  bool done_;  // Флаг, указывающий на завершение вычислений.
 };
 
 class WinogradExecutor {
@@ -50,9 +62,8 @@ class WinogradExecutor {
   WinogradExecutor() = default;
   ~WinogradExecutor() = default;
   Matrix Run(const Matrix& a, const Matrix& b, const int iterations) const {
-    Winograd winograd (a, b);
+    Winograd winograd(a, b);
 
-   
     for (int i = 0; i < iterations; ++i) {
       winograd.MultiplyMatrices();
     }
@@ -62,7 +73,7 @@ class WinogradExecutor {
   Matrix RunParallels(const Matrix& a, const Matrix& b, const int iterations,
                       unsigned int threads_amount) const {
     Winograd winograd(a, b);
- 
+
     for (int i = 0; i < iterations; ++i) {
       winograd.MultiplyMatricesInParallels(threads_amount);
     }
@@ -72,7 +83,7 @@ class WinogradExecutor {
   Matrix RunPipeline(const Matrix& a, const Matrix& b,
                      const int iterations) const {
     Winograd winograd(a, b);
-    
+
     for (int i = 0; i < iterations; ++i) {
       winograd.MultiplyMatricesInPipeline();
     }
